640f2ace4801431870178bc9782cbe8450f8a0c2
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-27 04:13:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,4 +6,4 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
 };
-updatePlaylist(playlist, artistName, songTitle);
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
