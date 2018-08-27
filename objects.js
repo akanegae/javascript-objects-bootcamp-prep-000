71d3b475045791f73ebec5d57a73e0aeb4b6200a
Alison Kanegae <alisonkanegae@gmail.com>
2018-08-27 03:40:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,14 @@ var playlist = {
   'Slowdive': 'Alison',
   'My Bloody Valentine': 'Sometimes'
 }
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, { [artistName]: songTitle})
+}
+const playlist = {
+  'Slowdive': 'Alison',
+  'My Bloody Valentine': 'Sometimes'
+}
+
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
+
+playlist;
